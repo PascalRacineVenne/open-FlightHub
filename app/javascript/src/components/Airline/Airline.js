4de42896a6116010c81d9ff6f30c991c7a77175a
Pascal Racine-Venne <pascalracinevenne@gmail.com>
2022-02-25 07:00:16 -0500

    react router not happy yet

---
+++ app/javascript/src/components/Airline/Airline.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Airline = () => {
+  return <div><h1>This is my Airlines#show page</h1></div>
+}
+
+export default Airline
---
+++ app/javascript/src/components/Airlines/Airlines.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Airlines = () => {
+  return (
+    <div>
+      <h1>This is my Airlines#index page</h1>
+    </div>
+    )
+}
+
+export default Airlines
